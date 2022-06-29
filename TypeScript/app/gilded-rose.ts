8ad22562742e6265484b872dc8efd55ae11ce6c5
Grecu Bogdan <BogdanOctavian.Grecu@softwire.com>
2022-06-29 16:03:26 +0100

    solved several of the comments of the previous pool request still needs some changes

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -59,7 +59,7 @@ export class GildedRose {
             }
           } else {
             if (this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1
+              this.items[i].quality = this.items[i].quality++;
             }
           }
         }
@@ -82,8 +82,9 @@ export class GildedRose {
   }
 
   updateSellIn(i){
-    if(this.items[i].name != 'Sulfuras, Hand of Ragnaros')
+    if(this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
       this.items[i].sellIn--;
+    }
   }
 
   updateQualitySulfuras(i){
@@ -95,9 +96,9 @@ export class GildedRose {
       this.items[i].quality = this.items[i].quality + 2;
     }
     else {
-      this.items[i].quality = this.items[i].quality + 1;
+      this.items[i].quality = this.items[i].quality++;
     }
-    this.items[i].quality = Math.min(this.items[i].quality,50)
+    this.items[i].quality = Math.min(this.items[i].quality,50);
   }
 
   updateQualityBackstage(i){
@@ -105,7 +106,7 @@ export class GildedRose {
       this.items[i].quality = 0;
     }
     else if(this.items[i].sellIn >= 10) {
-      this.items[i].quality = this.items[i].quality + 1;
+      this.items[i].quality = this.items[i].quality++;
     }
     else if(this.items[i].sellIn >= 5) {
       this.items[i].quality = this.items[i].quality + 2;
@@ -113,7 +114,7 @@ export class GildedRose {
     else if(this.items[i].sellIn >= 0) {
       this.items[i].quality = this.items[i].quality + 3;
     }
-    this.items[i].quality = Math.min(this.items[i].quality,50)
+    this.items[i].quality = Math.min(this.items[i].quality,50);
   }
 
   updateQualityConjured(i){
@@ -123,7 +124,7 @@ export class GildedRose {
     else {
       this.items[i].quality = this.items[i].quality - 2;
     }
-    this.items[i].quality = Math.max(this.items[i].quality,0)
+    this.items[i].quality = Math.max(this.items[i].quality,0);
   }
 
   updateQualityAnythingElse(i){
@@ -131,30 +132,31 @@ export class GildedRose {
       this.items[i].quality = this.items[i].quality - 2;
     }
     else {
-      this.items[i].quality = this.items[i].quality - 1;
+      this.items[i].quality = this.items[i].quality--;
     }
-    this.items[i].quality = Math.max(this.items[i].quality,0)
+    this.items[i].quality = Math.max(this.items[i].quality,0);
   }
 
   updateQuality2(){
-    for(let i = 0; i < this.items.length; i++) {
+    this.items.forEach((item) => {
+      let i = this.items.indexOf(item);
       this.updateSellIn(i);
-      if(this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
-        this.updateQualitySulfuras(i)
+      if(item.name === 'Sulfuras, Hand of Ragnaros') {
+        this.updateQualitySulfuras(i);
       }
-      else if(this.items[i].name == 'Aged Brie') {
+      else if(item.name === 'Aged Brie') {
         this.updateQualityAgedBrie(i);
       }
-      else if(this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+      else if(item.name === 'Backstage passes to a TAFKAL80ETC concert') {
         this.updateQualityBackstage(i);
       }
-      else if(this.items[i].name == 'Conjured Mana Cake') {
+      else if(item.name === 'Conjured Mana Cake') {
         this.updateQualityConjured(i);
       }
       else{
         this.updateQualityAnythingElse(i);
       }
-    }
-    return this.items
+    });
+    return this.items;
   }
 }
